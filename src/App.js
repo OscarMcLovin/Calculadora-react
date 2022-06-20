3932bf060f8b75148a081fa5baefc5644194df35
oscar <org734664@gmail.com>
2022-06-20 15:01:47 -0500

    Se finaliza el diseño, el funcionamiento de los botones y se entrega totalmente funcionando

--- src/App.js
+++ src/App.js
@@ -7,7 +7,19 @@ function App() {
   const ops = ['/', '*', '+', '-', '.'];
 
   const updateCalc = value => {
+      if (
+        ops.includes(value) && calc === '' ||
+        ops.includes(value) && ops.includes(calc.slice(-1)
+        )
+      ){
+        return;
+      }
+
       setCalc(calc + value);
+
+      if (!ops.includes(value)){
+        setResult(eval(calc + value).toString());
+      }
   }
 
   const createDigits = () => {
@@ -25,12 +37,28 @@ function App() {
      return digits;
   }
 
+  const calculate = () => {
+    setCalc(eval(calc).toString());
+  }
+
+  const deleteLast = () => {
+    if (calc == '') {
+      return;
+    }
+
+     const value = calc.slice(0, -1);
+
+     setCalc(value);
+
+
+  }
+
 
   return (
     <div className="App">
       <div className="calculator">
       <div className="display">
-        {result ? <span>(0)</span> : '' }
+        {result ? <span>({result})</span> : '' }
         { calc || "0" }
       </div>
 
@@ -44,7 +72,7 @@ function App() {
         <button onClick={() => updateCalc('-')}
         >-</button>
 
-        <button>DEL</button>
+        <button onClick={deleteLast}>DEL</button>
       </div>
 
       <div className="digits">
@@ -54,7 +82,7 @@ function App() {
         <button onClick={() => updateCalc('.')}>.
         </button>
 
-        <button >=</button>
+        <button onClick={calculate}>=</button>
       </div>
 
       </div>
